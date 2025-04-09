3b83672d8108e84e7c192a9f9127f5f69926405d
Liusuyi0517 <1353321409@qq.com>
2025-04-09 11:09:23 +0800

    Update SymPy_plot.py

--- src/SymPy_plot.py
+++ src/SymPy_plot.py
@@ -1,36 +1,38 @@
 import sympy as sp
-from sympy.plotting import plot, plot_implicit, plot3d_parametric_surface
 
-# Problem 1: 使用 plot 绘制 cos(tan(pi*x))
-def problem1():
-    x = sp.symbols('x')
+# 定义符号变量
+x, y, s, t = sp.symbols('x y s t')
+
+# 任务 1: 使用 sympy.plot 绘制函数曲线 cos(tan(pi * x))
+def plot_function():
     expr = sp.cos(sp.tan(sp.pi * x))
-    # TODO: 完成绘图，区间[-1, 1]
-    plot(expr, (x, -1, 1), xlabel='x', ylabel='cos(tan(pi*x))',
-         title='Problem 1: Plot of cos(tan(pi*x))')
+    p1 = sp.plot(expr, (x, -1, 1), title='Plot of cos(tan(pi * x))', xlabel='x', ylabel='y')
+    return p1
 
-# Problem 2: 使用 plot_implicit 绘制隐函数 e^y + cos(x)/x + y = 0
-def problem2():
-    x, y = sp.symbols('x y')
-    expr = sp.exp(y) + sp.cos(x)/x + y
-    # TODO: 完成隐函数绘图，选择合适的绘图区间防止除零错误，例如 x 从 0.1 开始
-    plot_implicit(expr, (x, -10, 10), (y, -10, 10),
-                  xlabel='x', ylabel='y',
-                  title='Problem 2: Implicit plot of exp(y)+cos(x)/x+y=0',
-                  points=500)
+# 任务 2: 使用 sympy.plot_implicit 绘制隐函数曲线 (e^y + cos(x)) / (x + y) = 0
+def plot_implicit_function():
+    expr = (sp.exp(y) + sp.cos(x)) / (x + y)
+    p2 = sp.plot_implicit(sp.Eq(expr, 0), (x, -5, 5), (y, -5, 5),
+                          title='Plot of (e^y + cos(x)) / (x + y) = 0',
+                          xlabel='x', ylabel='y')
+    return p2
 
-# Problem 3: 使用 plot3d_parametric_surface 绘制参数曲面
-def problem3():
-    s, t = sp.symbols('s t')
-    x = sp.exp(-s)*sp.cos(t)
-    y = sp.exp(-s)*sp.sin(t)
-    z = t
-    # TODO: 完成三维参数曲面绘图
-    plot3d_parametric_surface(x, y, z, (s, 0, 8), (t, 0, 5*sp.pi),
-                              xlabel='x', ylabel='y', zlabel='z',
-                              title='Problem 3: Parametric Surface Plot')
+# 任务 3: 使用 sympy.plot3d_parametric_surface 绘制参数曲面
+def plot_parametric_surface():
+    x_expr = sp.exp(-s) * sp.cos(t)
+    y_expr = sp.exp(-s) * sp.sin(t)
+    z_expr = t
+    p3 = sp.plot3d_parametric_surface(x_expr, y_expr, z_expr, (s, 0, 8), (t, 0, 5 * sp.pi),
+                                      title='Parametric Surface Plot',
+                                      xlabel='x', ylabel='y', zlabel='z')
+    return p3
+
 
 if __name__ == "__main__":
-    problem1()
-    problem2()
-    problem3()
+    # 绘制函数曲线
+    plot_function()
+    # 绘制隐函数曲线
+    plot_implicit_function()
+    # 绘制参数曲面
+    plot_parametric_surface()
+    
